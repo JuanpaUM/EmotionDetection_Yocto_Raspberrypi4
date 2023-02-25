4d701573ce6f47082fdbbc7f03c8dec691a87ba2
JuanpaUM <79667174+JuanpaUM@users.noreply.github.com>
2023-02-25 15:16:11 -0600

    Training

---
+++ Training/convert_h5_to_tflite.py
@@ -0,0 +1,16 @@
+
+"""
+Convert keras trained h5 model to tflite for deployment onto edge devices.
+
+"""
+
+import tensorflow as tf
+import keras
+from keras.models import load_model
+
+emotion_model = load_model('5cat_emotion_detection_model_100epochs.h5', compile=False)
+
+converter = tf.lite.TFLiteConverter.from_keras_model(emotion_model)
+#converter.optimizations = [tf.lite.Optimize.DEFAULT] #Uses default optimization strategy to reduce the model size
+tflite_model = converter.convert()
+open("5cat_emotion_detection_model_100epochs_no_opt.tflite", "wb").write(tflite_model)
